3d80e89758e47c0eb95eeb628c2246f2a4ec13d7
davihdsantos <davihenriqueds@gmail.com>
2017-06-20 19:33:52 -0300

    horizontal laser added to barcoDiferential model

--- src/freefloating_gazebo_demo/scripts/control_vel_dif.py
+++ src/freefloating_gazebo_demo/scripts/control_vel_dif.py
@@ -46,44 +46,44 @@ class VelocityCtrl():
         self.lin_vel_ang = 0
         self.ang_vel = 0
         self.kp_ang = 100 
-            self.ki_ang = 200
-            self.thruster_max = 30
-            self.vel_left = 0
-            self.vel_right = 0
-            self.thruster_command = numpy.array([0, 0])
+        self.ki_ang = 200
+        self.thruster_max = 30
+        self.vel_left = 0
+        self.vel_right = 0
+        self.thruster_command = numpy.array([0, 0])
 
-            self.pub_motor = rospy.Publisher('/barco_auv/thruster_command', JointState, queue_size=10)
+        self.pub_motor = rospy.Publisher('/barco_auv/thruster_command', JointState, queue_size=10)
 
-            rospy.Subscriber("/barco_auv/state", Odometry, self.get_usv_vel)  
-            rospy.Subscriber("/barco_auv/cmd_vel", Twist, self.get_target_vel)
+        rospy.Subscriber("/barco_auv/state", Odometry, self.get_usv_vel)  
+        rospy.Subscriber("/barco_auv/cmd_vel", Twist, self.get_target_vel)
 
-            while not rospy.is_shutdown():
-                self.pub_motor.publish(self.thruster_ctrl_msg(self.vel_ctrl(), thruster_name))
-                r.sleep()
+        while not rospy.is_shutdown():
+            self.pub_motor.publish(self.thruster_ctrl_msg(self.vel_ctrl(), thruster_name))
+            r.sleep()
 
-        def vel_ctrl(self):
-            self.lin_vel = self.target_vel.linear.x - self.usv_vel.twist.twist.linear.x
-            self.lin_vel = self.lin_vel * self.kp_lin + self.I_lin(self.lin_vel)
+    def vel_ctrl(self):
+        self.lin_vel = self.target_vel.linear.x - self.usv_vel.twist.twist.linear.x
+        self.lin_vel = self.lin_vel * self.kp_lin + self.I_lin(self.lin_vel)
 
-            if self.target_vel.angular.z != self.target_vel_ant.angular.z:
-                self.I_ant_ang = 0
+        if self.target_vel.angular.z != self.target_vel_ant.angular.z:
+            self.I_ant_ang = 0
 
-            if self.target_vel.angular.z == 0:
-                self.vel_left = self.lin_vel
-                self.vel_right = self.lin_vel
-            else: 
-                self.ang_vel = self.target_vel.angular.z - self.usv_vel.twist.twist.angular.z
-                erro = self.ang_vel
-            self.ang_vel = self.ang_vel * self.kp_ang + self.I_ang(self.ang_vel)
-            if self.ang_vel < 0:
-                self.vel_left = self.lin_vel + self.ang_vel
-                self.vel_right = self.lin_vel - self.ang_vel
-            if self.ang_vel > 0:
-                self.vel_right = self.lin_vel + self.ang_vel
-                self.vel_left = self.lin_vel - self.ang_vel
+        if self.target_vel.angular.z == 0:
+            self.vel_left = self.lin_vel
+            self.vel_right = self.lin_vel
+        else: 
+            self.ang_vel = self.target_vel.angular.z - self.usv_vel.twist.twist.angular.z
+            erro = self.ang_vel
+        self.ang_vel = self.ang_vel * self.kp_ang + self.I_ang(self.ang_vel)
+        if self.ang_vel < 0:
+            self.vel_left = self.lin_vel + self.ang_vel
+            self.vel_right = self.lin_vel - self.ang_vel
+        if self.ang_vel > 0:
+            self.vel_right = self.lin_vel + self.ang_vel
+            self.vel_left = self.lin_vel - self.ang_vel
             msg = "atual: {0}; desejada: {1}; erro: {2}; vel_left: {3}; vel_right: {4}; erro_bruto: {5}" .format(self.usv_vel.twist.twist.angular.z, self.target_vel.angular.z, self.ang_vel, self.vel_left, self.vel_right, erro)
             rospy.loginfo(msg)
-            
+        
         self.vel_left = self.sat_thruster(self.vel_left)
         self.vel_right = self.sat_thruster(self.vel_right)
 
