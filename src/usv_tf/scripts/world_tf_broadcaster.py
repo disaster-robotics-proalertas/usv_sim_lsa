dda757a71600c5dc7dbaae219d1f93158516ba18
davihdsantos <davihenriqueds@gmail.com>
2017-07-24 16:24:45 -0300

    minor changes

--- src/usv_tf/scripts/world_tf_broadcaster.py
+++ src/usv_tf/scripts/world_tf_broadcaster.py
@@ -2,15 +2,16 @@
 import roslib
 import rospy
 import tf
+import sys
 from nav_msgs.msg import Odometry
 
 def handle_turtle_pose(msg, br):
     # r = rospy.Rate(102)
-    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z), (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w), rospy.Time.now(), "base_link", "odom")
+    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z), (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w), rospy.Time.now(), sys.argv[1] + "/base_link", sys.argv[1] + "/odom")
     # r.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node("world_tf_broadcaster")
+    rospy.init_node(sys.argv[1] + "world_tf_broadcaster")
     br = tf.TransformBroadcaster()
     rospy.Subscriber("state", Odometry, handle_turtle_pose, br)
     rospy.spin()
