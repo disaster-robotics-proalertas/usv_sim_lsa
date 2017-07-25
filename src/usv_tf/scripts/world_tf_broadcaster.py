7f5a6a78b283d032481fcfcecd188955a5ea2bf6
davihdsantos <davihenriqueds@gmail.com>
2017-07-25 17:39:04 -0300

    multiple differential boats working. launch is diff_start_world.launch

--- src/usv_tf/scripts/world_tf_broadcaster.py
+++ src/usv_tf/scripts/world_tf_broadcaster.py
@@ -7,7 +7,7 @@ from nav_msgs.msg import Odometry
 
 def handle_turtle_pose(msg, br):
     # r = rospy.Rate(102)
-    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z), (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w), rospy.Time.now(), sys.argv[1] + "/base_link", sys.argv[1] + "/odom")
+    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z), (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w), rospy.Time.now(), sys.argv[1] + "_tf/base_link", sys.argv[1] + "_tf/odom")
     # r.sleep()
 
 if __name__ == '__main__':
