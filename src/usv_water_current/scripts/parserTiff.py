0b2b82f8332d137961b74fb972845f7a9ebd098d
marceloparavisi <marcelo.paravisi@osorio.ifrs.edu.br>
2017-12-31 20:06:36 -0200

    Started to integrate hec ras simulation file with usv_water_current package

--- src/usv_water_current/scripts/parserTiff.py
+++ src/usv_water_current/scripts/parserTiff.py
@@ -8,6 +8,7 @@ import yaml
 import math
 import scipy
 import rospy
+import sys
 
 from usv_water_current.srv import *
 from std_msgs.msg import String
@@ -18,13 +19,18 @@ from geometry_msgs.msg import Twist, Point, Quaternion
 from PIL import Image
 import matplotlib.pyplot as plt
 from osgeo import gdal
+import struct
 
 
 pub = rospy.Publisher('waterflow', OccupancyGrid, queue_size=1)
+mymap = OccupancyGrid();
+minSpeed= 0
+maxSpeed= 0
 
 def handleWaterCurrent(req):
 	global originX, originY, array
 #	print ("\n Received request",req.x,", ",req.y)
+
 	x = req.x-originX
 	if (x < 0):
 		x = 0
@@ -35,7 +41,10 @@ def handleWaterCurrent(req):
 		y = 0
 	if (y >= width):
 		y = width-1
+	rospy.logerr ("\n Received request %d, %d - (%d, %d) = %f",req.x,req.y,x,y,array[x][y])
 	print "\n[",x,",",y,"]: (",array[x][y],",",array[x][y],")"
+	if (array[x][y] < -100):
+		return GetSpeedResponse(0, 0)
 	return  GetSpeedResponse(array[x][y], array[x][y])
 #        return GetSpeedResponse(ux[req.x][req.y], uy[req.x][req.y])
 
@@ -45,6 +54,7 @@ def parse_config_file(config_file_name):
 	global originX
 	global originY
 	global width, height, dataset, array
+	global minSpeed, maxSpeed
 	with open(config_file_name, 'r') as stream:
         	data_loaded = yaml.load(stream)
 	print ('--------------------- Loading yaml file ', config_file_name)
@@ -54,8 +64,8 @@ def parse_config_file(config_file_name):
 	width = data_loaded['simulation']['width']
 	height = data_loaded['simulation']['height']
 	print (data_loaded['origin'])
-	print (data_loaded['origin'][0])
-	print (data_loaded['origin'][1])
+	print ("originx ",data_loaded['origin'][0])
+	print ("originy ",data_loaded['origin'][1])
 	print (data_loaded['image'])
 	print (data_loaded['resolution'])
 	dataset = gdal.Open(data_loaded['image'], gdal.GA_ReadOnly)
@@ -69,20 +79,24 @@ def parse_config_file(config_file_name):
 	for x in range(1, dataset.RasterCount + 1):
 		band = dataset.GetRasterBand(x)
 	    	array = band.ReadAsArray()
-	print "min: ", srcband.GetMinimum()
-	print "max: ", srcband.GetMaximum()
+	minSpeed = srcband.GetMinimum()
+	maxSpeed = srcband.GetMaximum()
+	print "min: ", minSpeed
+	print "max: ", maxSpeed
 	print "scale: ", srcband.GetScale()
 	print "unit_type: ", srcband.GetUnitType()
+
+
 
 def startRosService():
         s = rospy.Service('waterCurrent', GetSpeed, handleWaterCurrent)
         print "Ready to answer water current."
 
-def publishMap():
-	global array, width, height, originX, originY
+def loadMap():
+	global array, width, height, originX, originY, mymap, minSpeed, maxSpeed
 	mynorm = matplotlib.colors.Normalize(vmin=-0.1,vmax=0.1);
 
-	mymap = OccupancyGrid();
+	mymap.header.frame_id="odom";
 	mymap.info.resolution = resolution;
 	mymap.info.width = width;
 	mymap.info.height = height;
@@ -93,30 +107,40 @@ def publishMap():
 	mymap.info.origin.orientation.y = 0;
 	mymap.info.origin.orientation.z = 0;
 	mymap.info.origin.orientation.w = 1;
-	print "Preparing map"
-	for x in range(0,height):
-		for y in range(0,width):
-			value = 127*mynorm(array[x][y]);
-#	
+	delta = maxSpeed - minSpeed
+	rospy.logerr ("################### Preparing map. Size (%d, %d) origin(%d, %d)", width, height, originX, originY)
+	for x in xrange(height-1, -1, -1):
+#	for x in range(0, height):
+		for y in range(0, width):
+			value = 127*array[x][y]/delta;
 			if (value >127):
 				value = 127;
 			elif (value < 0):
 				value = 0;
 			mymap.data.append(int(value));			
-	print "Sending map"
-	pub.publish(mymap);
+	rospy.logerr("#################### Map loaded!")
+	
 
 if __name__ == '__main__':
-	print "Hello!"
+	global mymap
 	rospy.init_node('usv_water_current')
+	rospy.logerr ("###################starting usv_water_current. argument count: %d", len(sys.argv))
+	if (len(sys.argv)!=2 and len(sys.argv)!=4):
+		rospy.logerr ("###################Error: argument count invalid! %d", len(sys.argv))
+		for i in range(0, len(sys.argv)):
+			rospy.logerr(" argv %d : %s", i, sys.argv[i])
+		exit(0)
 	config_file_name = sys.argv[1]
 	parse_config_file(config_file_name)
 	startRosService();
-	rate = rospy.Rate(10) # 10hz
+	loadMap()
+	rate = rospy.Rate(1) # 10hz
 	while not rospy.is_shutdown():
-		publishMap()
+		rospy.logerr("---sending map! %d, %d", mymap.info.origin.position.x, mymap.info.origin.position.y)
+		pub.publish(mymap);
+		rospy.logerr("---map sent!")
 		rate.sleep()
-
+	print "Closing usv_water_current."
 
 
 	
