c80aeff209cafa46baa7920f1afb6d1ae39af115
marceloparavisi <marcelo.paravisi@osorio.ifrs.edu.br>
2019-01-10 13:47:40 -0200

    Update dependencies.sh

--- .usv_sim_ci_config/dependencies.sh
+++ .usv_sim_ci_config/dependencies.sh
@@ -17,5 +17,8 @@
 # DEP PACKAGES
 apt -qq install --no-install-recommends --allow-unauthenticated -y \
   build-essential python-catkin-tools python-pip dvi2ps dvipng binutils \
-  mesa-utils module-init-tools x-window-system ros-$ROS_DISTRO-python-orocos-kdl
+  mesa-utils module-init-tools x-window-system ros-$ROS_DISTRO-python-orocos-kdl \
+  libfftw3-* libxml++2.6-* libsdl-image1.2-dev libsdl-dev python-rosinstall python-rosinstall-generator \
+  python-wstool build-essential python-rosdep python-wxtools python-lxml python-pathlib python-h5py \
+  python-scipy python-geolinks python-gdal
 
